05c0b53bcae7da09f88673c935966e0f182f50f8
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2019-02-01 11:17:26 +0100

    Scalastyle checks

--- src/test/scala/at/ac/ait/clustering/MultipleInputClusteringTest.scala
+++ src/test/scala/at/ac/ait/clustering/MultipleInputClusteringTest.scala
@@ -1,65 +1,67 @@
 package at.ac.ait.clustering
 
-import org.scalatest.FlatSpec
-import org.scalatest._
 import org.scalactic.source.Position.apply
+import org.scalatest._
+import org.scalatest.FlatSpec
 import scala.collection.Seq
 
 class MultipleInputClusteringTestSpec extends FlatSpec {
 
   def computeClusters(inputs: Iterable[Iterable[Int]]) = {
     val results = MultipleInputClustering.getClustersMutable(inputs.toIterator).toSet
-    (inputs.flatten.toSet, results.map(_.id).toSet, results.map(_.cluster).toSet)    
+    (inputs.flatten.toSet, results.map(_.id).toSet, results.map(_.cluster).toSet)
   }
-  
+
   "Disjoint input sets" should "yield one cluster for each set" in {
     val inputs = Set(Set(1), Set(2), Set(3))
     val (inputIds, resultIds, resultClusterIds) = computeClusters(inputs)
     assert(resultIds.size == inputIds.size)
     assert(resultClusterIds.size == inputIds.size)
   }
-  
+
   "Two intersecting input sets" should "be joined into one cluster" in {
     val inputs = Set(Set(1,2), Set(2,3))
     val (inputIds, resultIds, resultClusterIds) = computeClusters(inputs)
     assert(resultIds.size == inputIds.size)
-    assert(resultClusterIds.size == 1)    
+    assert(resultClusterIds.size == 1)
   }
-  
+
   "Three intersecting input sets" should "be joined into one cluster" in {
     val inputs = Set(Set(1,2), Set(2,3), Set(3,4))
     val (inputIds, resultIds, resultClusterIds) = computeClusters(inputs)
     assert(resultIds.size == inputIds.size)
-    assert(resultClusterIds.size == 1)    
+    assert(resultClusterIds.size == 1)
   }
-  
+
   "Five input sets with one intersection" should "yield four clusters" in {
     val inputs = Set(Set(1), Set(2), Set(3), Set(4), Set(4, 5))
     val (inputIds, resultIds, resultClusterIds) = computeClusters(inputs)
     assert(resultIds.size == inputIds.size) // All input Ids are clustered
     assert(resultClusterIds.size == 4)
   }
-  
+
   "Merging 123, 456, 789 and 34" should "result in two clusters of size 6 and 3" in {
     val input_sets = Set(Set(1, 2, 3), Set(4, 5, 6), Set(7, 8, 9), Set(3, 4))
     // Clustering:
     val results = MultipleInputClustering.getClustersImmutable(input_sets.toIterator).toSet
     val clusters = results
-      .groupBy(_.cluster) // group by cluster identifier
-      .mapValues(_.map(_.id)) // map from  Map[cluster -> List[Result(id,cluster)]] to Map[cluster -> List[id]]      
+      // group by cluster identifier
+      .groupBy(_.cluster)
+      // map from Map[cluster -> List[Result(id,cluster)]] to Map[cluster -> List[id]]
+      .mapValues(_.map(_.id))
     val clusterSizes = clusters
       .mapValues(_.size).values.toList
     assert(clusterSizes.contains(6))
     assert(clusterSizes.contains(3))
   }
-  
+
   "Input set sequences" should "also work" in {
     val inputs = Seq(Seq(1,2), Seq(2,3), Seq(3,4))
     val (inputIds, resultIds, resultClusterIds) = computeClusters(inputs)
     assert(resultIds.size == inputIds.size)
     assert(resultClusterIds.size == 1)
   }
-  
+
   "Both algorithms" should "produce identical results and the mutable should be faster" in {
     // Timer
     def time[R](msg: String = "Elapsed")(block: => R): (Long, R) = {
@@ -71,12 +73,12 @@ class MultipleInputClusteringTestSpec extends FlatSpec {
       (t1 - t0, result)
     }
     // Data generation
-    def generateTestData(num_sets: Int, num_addrs: Int, max_addrs: Int) = {
+    def generateTestData(numSets: Int, numAddrs: Int, maxAddrs: Int) = {
       val r = scala.util.Random
-      val more_sets = (1 to num_sets) //
-        .map(_ => 1 to (2 + r.nextInt(max_addrs - 2))) // number of elements
-        .map(x => x.map(_ => r.nextInt(num_addrs)).toSet).toSet // map to addrs
-      more_sets
+      val moreSets = (1 to numSets) //
+        .map(_ => 1 to (2 + r.nextInt(maxAddrs - 2))) // number of elements
+        .map(x => x.map(_ => r.nextInt(numAddrs)).toSet).toSet // map to addrs
+      moreSets
     }
 
     val TESTSIZE = 1E4.toInt
